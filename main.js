7224c11f14e1a40acd406b15f1fc8f0e7db24b08
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-03 11:30:21 +0100

    iniziate classi statiche

---
+++ main.js
@@ -0,0 +1,6 @@
+// Le classi che contengono funzioni e non proprietà (come la classe Math, per esempio), quindi non vanno istanziate, si chiamano: Statiche
+
+const result = Math.sqrt(64);
+
+console.log(result);
+
