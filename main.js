c8d1e8599cca542b732694cb8b601559046720a9
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-03 15:50:46 +0100

    implementati metodi classe statica in classe Point

--- main.js
+++ main.js
@@ -6,10 +6,40 @@
 /*****************************************************************************/
 
 const pointA = new Point(3, 5);
-
 const pointB = new Point(6, 1);
 
+/*--*/
+
+const pointC = new Point(7, 1);
+const pointD = new Point(3, 6);
+const pointE = new Point(1, 1);
+const pointF = new Point(-2, 1);
+
+
+// Metodi applicati dalla classe astratta MathC
 
 const distance = MathC.calculateDistance(pointA, pointB);
+const medianPoint = MathC.medianPoint(pointA, pointB);
+const distanceOrigin = MathC.distanceFromOrigin(pointA);
+const nearest = MathC.nearestPoint(pointA, pointB, pointC, pointD, pointE, pointF);
 
-console.log(distance);
+console.log("METODI DELLA CLASSE MathC.\n\n");
+console.log("- Distanza tra A e B: ", distance);
+console.log("- Punto medio tra A e B: ", medianPoint);
+console.log("- Distanza tra A e origine: ", distanceOrigin);
+console.log("- Più vicino ad A tra B -> F: ", nearest);
+
+/****/
+
+// Metodi applicati dalla classe Point
+
+const distanceP = pointA.distanceFrom(pointB);
+const medianPointP = pointA.medianPointFromPoint(pointB);
+const distanceOriginP = pointA.distanceFromOrigin();
+const nearestP = pointA.nearestPoint(pointB, pointC, pointD, pointE, pointF);
+
+console.log("\nMETODI DELLA CLASSE Point.\n\n");
+console.log("- Distanza tra A e B: ", distanceP);
+console.log("- Punto medio tra A e B: ", medianPointP);
+console.log("- Distanza tra A e origine: ", distanceOriginP);
+console.log("- Più vicino ad A tra B -> F: ", nearestP);
--- mathC.js
+++ mathC.js
@@ -2,23 +2,42 @@ class MathC { // Siccome questa classe non va instanziata, non ha bisogno di cos
 
     static calculateDistance(A, B) { // Per applicare un metodo di una classe che non viene istanziata all'inizio va messo "static"
         // La distanza tra due punti nel piano cartesiano è: radice quadrata((xB - XA)^2 + (yB - yA)^2)
-        
         const xDif = B.x - A.x;
-
         const xDif2 = xDif * xDif;
-
         const yDif = B.y - A.y;
-
         const yDfi2 = yDif * yDif;
-
         const sumDif2 = xDif2 + yDfi2;
-
         const sqrtSumDif2 = Math.sqrt(sumDif2);
-
         return sqrtSumDif2;
-
         //return Math.sqrt(Math.pow(B.x - A.x, 2) + Math.pow(B.y - A.y, 2));
+    }
 
+    static medianPoint(A, B){
+        const xM = (A.x + B.x)/2
+        const yM = (A.y + B.y)/2
+        const median = new Point(xM, yM);
+        return median;
+    }
+
+
+    static distanceFromOrigin(p){
+        const origin = new Point(0,0);
+        const distance = this.calculateDistance(origin, p);
+        return distance;
+    }
+
+
+    static nearestPoint(startingPoint, ...points){
+        let nearest = points[0];
+
+        for (let i = 1; i  < points.length; i++) {
+            const point = points[i];
+            if (this.calculateDistance(startingPoint, point) < this.calculateDistance(startingPoint, nearest)) {
+                nearest = point;
+            }
+        }
+
+        return nearest;
     }
 
 }
